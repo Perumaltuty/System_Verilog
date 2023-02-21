093de79caad13bdca5ad79f093ab7395d0a74243
Perumal A <112106273+Perumaltuty@users.noreply.github.com>
2023-02-21 19:04:12 +0530

    Create union_packed.sv

---
+++ data_types/union/union_packed.sv
@@ -0,0 +1,14 @@
+typedef union packed {
+  bit [7:0] B;
+  logic [7:0] C;
+} ABC_u;
+module union_packed();
+  ABC_u abc; 
+  initial begin 
+  abc.B = 'hab;
+    $display("\n abc.B = %0h" ,abc.B);
+  abc.C = 'hcd;
+    $display("\n abc.C = %0h" ,abc.C); 
+    $display("\n size is:",$bits(ABC_u));
+  end 
+endmodule

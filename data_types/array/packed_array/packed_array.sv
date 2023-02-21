c28e9947230dd3ab0eadcde12df45820919b7b66
Perumal A <112106273+Perumaltuty@users.noreply.github.com>
2023-02-21 15:59:03 +0530

    Create packed_array.sv

---
+++ data_types/array/packed_array/packed_array.sv
@@ -0,0 +1,24 @@
+`timescale 1ns / 1ps
+module packed_array;
+  bit [3:0]abc;
+  logic [15:0]pqr;
+  reg [7:0]xyz;
+ initial begin
+abc = 4'b0110;
+  pqr = 16'h10fe;
+  xyz = 8'd16;
+   $display("\n displaying the bit data type values");
+  foreach(abc[i]) begin
+    $display("data of abc[%0d] = %0b",i,abc[i]);
+  end
+   $display("\n displaying the logic data type values");
+  foreach(pqr[i]) begin
+    $display("data of pqr[%0d] = %0b",i,pqr[i]);
+  end
+  $display("\n displaying the reg data type values");
+  foreach(xyz[i]) begin
+    $display("data of xyz[%0d] = %0b",i,xyz[i]);
+  end
+ end
+
+endmodule 

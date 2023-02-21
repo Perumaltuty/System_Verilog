3f2ca46c0600d2486c0f5c0f0a9be44fe46ac1ab
Perumal A <112106273+Perumaltuty@users.noreply.github.com>
2023-02-21 10:45:16 +0530

    Create real_to_time.sv

---
+++ data_types/casting of data types/real_to_time.sv
@@ -0,0 +1,21 @@
+`timescale 1ns / 1ps
+
+
+module real_to_time();
+
+ real real_data;
+ time time_data;
+
+ initial begin
+
+   real_data = 8.04; 
+
+   $display("\nbefore casting real_data = %0.4f",real_data);
+
+   time_data = time'(real_data); 
+
+   $display("after casting real to time = %0t\n",time_data);
+
+ end
+endmodule 
+

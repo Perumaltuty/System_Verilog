ed952ee8f14300844f06ad8329ac6520ff397a01
Perumal A <112106273+Perumaltuty@users.noreply.github.com>
2023-02-22 19:08:55 +0530

    Create foreach_loop.sv

---
+++ day_2/loops/foreach/foreach_loop.sv
@@ -0,0 +1,12 @@
+`timescale 1ns / 1ps
+module foreach_loop();
+  int array[5];
+  initial 
+  begin
+       foreach(array[i])
+    begin 
+          array[i]=i;
+      $display("\tarray[%0d]=%0d",i,array[i]);
+    end 
+  end 
+endmodule 

18c1f01bb27422c8d990a5a811d56f407a961dab
Perumal A <112106273+Perumaltuty@users.noreply.github.com>
2023-02-22 16:41:45 +0530

    Create case_range.sv

---
+++ day_2/conditional/case_range.sv
@@ -0,0 +1,17 @@
+`timescale 1ns / 1ps
+module case_range;
+int x;
+initial begin
+  x =10;
+     case(x) inside
+    [2:3] : $display("Value of x = %0d", x);
+    [4:5] :$display("Value of x = %0d",x);
+    [6:9] : $display("Value of x = %0d",x);
+    [8:9]  : $display("Value of x = %0d" ,x);
+    default : $display("Value of x is not find");
+  endcase
+
+end
+
+endmodule
+

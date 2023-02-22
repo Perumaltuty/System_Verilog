9facc9f13dea60fb1e0ab231a7a15d74acb1d63e
Perumal A <112106273+Perumaltuty@users.noreply.github.com>
2023-02-22 19:02:14 +0530

    Create do_while.sv

---
+++ day_2/loops/do_while.sv
@@ -0,0 +1,12 @@
+`timescale 1ns / 1ps
+ module do_while_code; 
+ int apple = 1;
+ initial begin 
+           do 
+           begin
+                 $display("\t Value of apple = %0d", apple);
+                     apple = apple +1;
+                       end
+                       while(apple<6); 
+                     end
+                     endmodule 

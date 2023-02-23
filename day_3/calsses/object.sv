682273e947ef5f3d333dff7452aaa1fd8a454753
Perumal A <112106273+Perumaltuty@users.noreply.github.com>
2023-02-23 15:08:21 +0530

    Create object.sv

---
+++ day_3/calsses/object.sv
@@ -0,0 +1,25 @@
+`timescale 1ns / 1ps
+class home_1; //define class
+  bit light;
+  int fan ;
+  string switch;
+  task open_electricity();
+    switch = "ON";
+    $display("switch is %s so electricity is open",switch);
+  endtask
+endclass
+module tb;
+
+  home_1 h1; 
+   initial begin
+    $display("Using new() method we can create an object");
+        h1=new();
+    if (h1==null)
+      $display("object is empty");
+    else
+      $display("object is not empty");
+  end 
+endmodule
+
+//Using new() method we can create an object
+//object is not empty

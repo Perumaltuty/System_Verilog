1a810a502f9d8e47b8ba59fb2770b0f02cf2d125
Perumal A <112106273+Perumaltuty@users.noreply.github.com>
2023-02-24 12:40:33 +0530

    Create anstract.sv

---
+++ day_3/calsses/abstraction/anstract.sv
@@ -0,0 +1,27 @@
+`timescale 1ns / 1ps
+virtual class A;
+  int a = 5;
+  function void disp();
+    $display("1.Value of a = %0d",a);
+  endfunction   
+endclass
+class B extends A;
+  int a = 6;
+  function void display();
+    $display("2.Value of a = %0d",a);
+  endfunction                          
+endclass                                                   
+module abstract_ex();
+   B b1;
+  initial begin
+    b1 = new;
+    b1.a = 10;
+    b1.disp();
+    b1.display();                                                                                      
+  end
+endmodule
+
+
+//1.Value of a = 5
+//2.Value of a = 10
+//An abstract class cannot be instantiated, it can only be derived.

a0517d48a1f04895b98bc437121168b183aded0e
Perumal A <112106273+Perumaltuty@users.noreply.github.com>
2023-02-24 10:42:44 +0530

    Create value.sv

---
+++ day_3/calsses/parameterise/value.sv
@@ -0,0 +1,28 @@
+`timescale 1ns / 1ps
+class mirafra #(parameter branch,employes);
+  bit [branch-1:0]b1;
+  bit [employes-1:0]b2;
+   function new();
+    b1=13;
+    b2=9;
+   endfunction
+   function void disp();
+    $display("b1=%0d,b2=%0d",b1,b2);
+   endfunction
+endclass
+ mirafra#(3,2) m;
+module value;
+  initial begin
+    m=new();
+    $display("");
+    $display("contents of m ");
+    m.disp();
+    $display("");
+  end
+endmodule
+
+
+
+
+//contents of m 
+//b1=5,b2=1
